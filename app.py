210fdf986a613f61718d71c248119ca6864a7031
Siva Prakash <prakashdeveloper@outlook.in>
2022-06-30 07:56:50 +0530

    Updated app.py & README.md files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+import json
 from datetime import date
 import numpy as np
 import pickle
@@ -26,19 +26,19 @@ with st.sidebar:
 if selected == "Iris Classification":
     modeliris = pickle.load(open("models/iris_model.pkl", "rb"))
     st.title("Iris Classification")
-    sl = st.number_input("Sepal Length")
-    sw = st.number_input("Sepal Width")
-    pl = st.number_input("Petal Length")
-    pw = st.number_input("Petal Width")
+    sl = st.number_input("Sepal Length", min_value=4.3, max_value=7.9, step=0.1)
+    sw = st.number_input("Sepal Width", min_value=3.1, max_value=4.4, step=0.1)
+    pl = st.number_input("Petal Length", min_value=1.2, max_value=6.9, step=0.1)
+    pw = st.number_input("Petal Width", min_value=0.1, max_value=2.5, step=0.1)
     X = np.array([sl, sw, pl, pw]).reshape(1, -1)
     if st.button("Predict"):
         prediction = modeliris.predict(X)
-        st.success(prediction)
+        st.success(f"Classified Iris Species is {prediction[0]}")
 
 elif selected == "Heart Diasease Prediction":
     st.title("Heart Diasease Classification")
     modelheart = pickle.load(open("models/heart_disease_model.pkl", "rb"))
-    age = st.number_input("Age")
+    age = st.number_input("Age", min_value=18, max_value=80)
     sex = st.selectbox("Sex", ["Male", "Female"])
     cp = st.selectbox(
         "Chest Pain Type",
@@ -124,7 +124,10 @@ elif selected == "Heart Diasease Prediction":
     )
     if st.button("Predict"):
         prediction = modelheart.predict(data)
-        st.success(prediction)
+        if prediction == 1:
+            st.success("Sorry, You have Heart Diasease 😞")
+        else:
+            st.success("Hey, You don't have Heart Diasease 😎")
 
 elif selected == "Car Price Prediction":
     st.title("Car Price Prediction")
@@ -166,9 +169,31 @@ elif selected == "Car Price Prediction":
     )
     if st.button("Predict"):
         prediction = np.round(modelcar.predict(prediction), 2)
-        st.success(f"Car can be sell for {prediction}")
+        st.success(f"You sell the car for {prediction[0]} lakhs")
 
 
 else:
     st.title("House Price Prediction")
     modelhouse = pickle.load(open("models/house_predict_model.pkl", "rb"))
+    with open("columns.json", "r") as f:
+        columns = json.load(f)["data_columns"]
+    location = st.selectbox("Location", columns[8:-1])
+    area_type = st.selectbox("Area Type", columns[4:8])
+    bhk = st.number_input("No of bedrooms", min_value=1, max_value=5)
+    total_sqft = st.number_input("Total Square feet", min_value=10000, max_value=500000)
+    bath = st.number_input("No of bathroom", min_value=1, max_value=5)
+    balcony = st.number_input("No of Balcony", min_value=1, max_value=5)
+
+    area_type_ind = columns.index(area_type)
+    location_ind = columns.index(location)
+    data = np.zeros(len(columns))
+    data[0] = bhk
+    data[1] = total_sqft
+    data[2] = bath
+    data[3] = balcony
+    data[area_type_ind] = 1
+    data[location_ind] = 1
+
+    if st.button("Predict"):
+        prediction = np.round(modelhouse.predict([data])[0], 2)
+        st.success(f"You sell the house for {prediction} lakhs")
